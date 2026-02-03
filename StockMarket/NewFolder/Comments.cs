2f22624f6851ce06c9d1f5d9a4bda4f0b84aedd1
Shobhit Singh <shobhitsingh@quantumphinance.com>
2026-02-03 08:38:54 +0530

    Model Creation

---
+++ StockMarket/NewFolder/Comments.cs
@@ -0,0 +1,12 @@
+﻿namespace StockMarket.NewFolder
+{
+    public class Comments
+    {
+        public int ID { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public int? StockID { get; set; }
+        public Stock? Stock { get; set; }
+    }
+}
---
+++ StockMarket/NewFolder/Stock.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StockMarket.NewFolder
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public string Symbol { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Purchanse { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal LastDiv { get; set; }
+        public string Industry { get; set; } = string.Empty;
+        public long MarketCap { get; set; }
+        public List<Comments> Comments { get; set; } = new List<Comments>();
+    }
+}
