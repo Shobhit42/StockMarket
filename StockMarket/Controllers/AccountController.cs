8e3c5f06a1f732cbfd31d4a8e1a06d7e32da1456
Shobhit Singh <singh.shobhit4225@gmail.com>
2026-02-15 22:29:02 +0530

    Login Implementation

--- StockMarket/Controllers/AccountController.cs
+++ StockMarket/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using StockMarket.Dtos;
 using StockMarket.Models;
 using StockMarket.Services;
@@ -11,11 +12,30 @@ namespace StockMarket.Controllers
     public class AccountController : ControllerBase
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
         private readonly ITokenService _tokenService;
-        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ITokenService tokenService)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
             _tokenService = tokenService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            if(!ModelState.IsValid) 
+                return BadRequest(ModelState);
+
+            var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username);
+            if (user == null)
+                return Unauthorized("Invalid UserName");
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+            if (!result.Succeeded)
+                return Unauthorized("UserName not found and/or password incorrect");
+
+            return Ok(new NewUserDto { UserName = user.UserName, Email = user.Email, Token = _tokenService.CreateToken(user)});
         }
 
         [HttpPost("register")]
