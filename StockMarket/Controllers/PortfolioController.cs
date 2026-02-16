764626a046267e45ec71a273dd8c5c932925d94f
Shobhit Singh <singh.shobhit4225@gmail.com>
2026-02-16 22:52:08 +0530

    Partial Implementation of Portfolio controller

---
+++ StockMarket/Controllers/PortfolioController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StockMarket.Interfaces;
+using StockMarket.Models;
+
+namespace StockMarket.Controllers
+{
+    [Route("api/portfolio")]
+    [ApiController]
+    public class PortfolioController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IStockRepository _stockRepository;
+        public PortfolioController(UserManager<AppUser> userManager, IStockRepository stockRepository)
+        {
+            _userManager = userManager;
+            _stockRepository = stockRepository;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetUserPortfolio()
+        {
+            var username = User.Identity.Name;
+            if (username == null)
+                return Unauthorized();
+
+            var user = await _userManager.FindByNameAsync(username);
+
+            if (user == null)
+                return NotFound();
+        }
+
+    }
+}
---
+++ StockMarket/Extentions/ClaimsExtentions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace StockMarket.Extentions
+{
+    public static class ClaimsExtentions
+    {
+        public static string GetUsername(this ClaimsPrincipal principal)
+        {
+            return principal.Claims.SingleOrDefault(x => x.Type.Equals("")).Value;
+        }
+    }
+}
